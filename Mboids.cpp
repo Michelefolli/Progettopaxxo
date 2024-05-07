4ef17bbb1b536d40c3671a105c3d6d25f346310d
Michele Folli <mfolli04@gmail.com>
2024-05-07 11:44:51 +0200

    struttura progetto

---
+++ Mboids.cpp
@@ -0,0 +1,28 @@
+/* #include vari
+
+Boid struct
+Struct stats
+
+Classe Stormo{
+vector<Boid> (matrice)
+params
+
+ciclo per ogni elemento del vettore Stormo
+    funzioni correzione velocità
+        aggiornamento 
+
+funzione statistiche
+}
+
+#main
+
+get params (s, a, c, d, d_s, n, T=60s, d_t=1s)
+Generazione casuale in un altro file
+push_back dei boid per ogni 1->n
+
+ciclo per il tempo 1->60
+    ciclo per ogni elemento della matrice Stormo 1->n
+
+vettore<stats> che contiene le statistiche ogni tot tempo
+cout tutto
+*/
